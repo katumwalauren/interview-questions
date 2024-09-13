c9476908db0b10890737e220709ac4e697af9d10
katumwalauren <95852478+katumwalauren@users.noreply.github.com>
2024-09-13 13:44:48 +0300

    new

---
+++ firstone/general.js
@@ -0,0 +1,76 @@
+// What did you learn yesterday/this week?
+I have studied strapi that has helped me to learn more of the git commands 
+// What excites or interests you about coding?
+The most exiting this is how a machine helps me to solve problems and the creativity in coding
+// What is a recent technical challenge you experienced and how did you solve it?
+i was working on tempereture app and i needed to implement a dynamic formulars based on user input. 
+so i broke down the code to handling the problem step by step which led to 
+display relevant fields based on user selection. 
+// When building a new web site or maintaining one, can you explain some techniques you have used to increase performance?
+1 -> use of HTTP/2 which helps to enable faster page loading depending on the speed 
+2 -> perfecting of font that helps to avoid blank testing during font loading
+3 -> combining of files like javascript, css to reduce the number of HTTP requests
+4 -> break down of javascript or typescript depending on the app interaction which helps to incrase speed
+// Can you describe some SEO best practices or techniques you have used lately?
+1 -> Analyzing and monitoring performance of the project eg the use of google
+2 ->  regularly updating content for better visuals
+3 -> optimizing user experience to improve the users engagment in the project
+// Can you explain any common techniques or recent issues solved in regards to front-end security?
+cors -> this is configured to allow trusted organs access the APIs
+Escape output -> Always escape dynamic data before insering into HTML
+Password stortage -> ensure  passwords are made by strong algorithims 
+// What actions have you personally taken on recent projects to increase maintainability of your code?
+Modulizing code -> In projects like those using Next.js and React, I’ve broken down the UI into reusable components. For example, in the cargo management website, components such as headers, footer.
+custom rule -> configured custom rules to align with the specific needs of the project
+Type dedination -> This ensures all team members work with the same data structures 
+Separation of Concerns ->  Ensured clear separation between logic, styles, and markup.
+// Talk about your preferred development environment.
+Extensions ->  I use a variety of extensions to improve my workflow
+Backup and Cloud Storage-> i use this to backup my documents of different projects
+Browser Tools -> used to  measure and improve performance and accessibility.
+chakara-todo-list. It provides built-in accessibility features and makes styling easier through a component-based API.
+MongoDB Compass -> For working with MongoDB databases. These tools allow me to visualize and interact with database documents, especially helpful in API development.
+
+// Which version control systems are you familiar with?
+Github
+// Can you describe your workflow when you create a web page?
+-> Gather Requirements: Collect information on what features are needed and ideas on how the project should look like
+-> intall the neccessary extentions 
+-> set up development environment eg Set Up Framework(HTML, CSS), Install Dependencies(axios, react etc)
+-> Build Layout(HTML/CSS,javasvcript) 
+-> testing the code in a browser like chrome, firefox  so that ican test it's performance
+->  Make a code review  to catch issues and ensure code quality    
+->                                                                                                                     )
+// If you have 5 different stylesheets, how would you best integrate them into the site?
+Combining  the 5 stylesheets into one or two files to reduces the number of HTTP requests,
+Minify the combined CSS to reduce its size and improve load times.
+// Can you describe the difference between progressive enhancement and graceful degradation?
+Progressive enhancement is a development strategy that focuses on building a basic, functional version of a website that works for all users, regardless of their device
+while 
+Graceful degradation focuses on building a rich, fully-featured experience for modern browsers and then ensuring that, when viewed on older or 
+less capable browsers, the website still works, even if some features are downgraded or missing.
+// How would you optimize a website's assets/resources?
+<img src="image-600.jpg" 
+     srcset="image-400.jpg 400w, image-600.jpg 600w, image-1200.jpg 1200w" 
+     alt="Example">
+
+// How many resources will a browser download from a given domain at a time?
+6-8 resources
+// What are the exceptions?
+// Name 3 ways to decrease page load (perceived or actual load time).
+// If you jumped on a project and they used tabs and you used spaces, what would you do?
+// Describe how you would create a simple slideshow page.
+// If you could master one technology this year, what would it be?
+// Explain the importance of standards and standards bodies.
+// What is Flash of Unstyled Content? How do you avoid FOUC?
+// Explain what ARIA and screenreaders are, and how to make a website accessible.
+// Explain some of the pros and cons for CSS animations versus JavaScript animations.
+// What does CORS stand for and what issue does it address?
+// How did you handle a disagreement with your boss or your collaborator?
+// What resources do you use to learn about the latest in front end development and design?
+// What skills are needed to be a good front-end developer?
+// What role do you see yourself in?
+// Can you explain what happens when you enter a URL into the browser?
+// Describe the difference between SSR and CSR. Discuss the pros and cons.
+// Are you familiar with static rendering?
+// Rehydration?
